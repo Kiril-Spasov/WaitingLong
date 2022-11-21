a6607b5104954b2141738eaa41d53eaec9be56dc
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-21 14:53:15 +0200

    Add project files.

---
+++ WaitingLong/FormWaitingLong.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WaitingLong
+{
+    partial class FormWaitingLong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCalculate = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnCalculate
+            // 
+            this.BtnCalculate.Location = new System.Drawing.Point(60, 68);
+            this.BtnCalculate.Name = "BtnCalculate";
+            this.BtnCalculate.Size = new System.Drawing.Size(210, 65);
+            this.BtnCalculate.TabIndex = 0;
+            this.BtnCalculate.Text = "Calculate";
+            this.BtnCalculate.UseVisualStyleBackColor = true;
+            this.BtnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(348, 68);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(358, 321);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormWaitingLong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnCalculate);
+            this.Name = "FormWaitingLong";
+            this.Text = "Waiting Long";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCalculate;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ WaitingLong/FormWaitingLong.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WaitingLong
+{
+    public partial class FormWaitingLong : Form
+    {
+        public FormWaitingLong()
+        {
+            InitializeComponent();
+        }
+
+        //Those are the hours and minutes for each line.
+        int hours = 0;
+        int mins = 0;
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            string line1 = "";
+            string line2 = "";
+
+            int timeIntervalInMins = 0;
+
+            string path = Application.StartupPath + @"\wait.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            bool finished = false;
+
+            while (!finished)
+            {
+                line1 = streamReader.ReadLine();
+                line2 = streamReader.ReadLine();
+                
+                if (line1 == "0" || line2 == "0")
+                {
+                    finished = true;
+                }
+                else
+                {
+                    int timeValue1 = DetermineTimeValue(line1);
+                    TxtResult.Text += $"From {hours}:{mins} to ";
+
+                    int timeValue2 = DetermineTimeValue(line2);
+                    TxtResult.Text += $"{hours}:{mins}";
+
+                    //We check if the first time is larger than the second.
+                    //We check how many mins till midnight and then add the mins after midnight.
+                    //Example: first time is 21:00 and second is 07:00,
+                    //There are 180 mins till midnight and 420 mins after.
+                    if (timeValue1 > timeValue2)
+                    {
+                        timeIntervalInMins = (1440 - timeValue1) + timeValue2;
+                    }
+                    else
+                    {
+                        timeIntervalInMins = timeValue2 - timeValue1;
+                    }
+
+                    TxtResult.Text += " is " + timeIntervalInMins + " minutes " + Environment.NewLine;
+                }
+            }
+        }
+
+        private int DetermineTimeValue(string time)
+        {
+            //As per the assigment, each line in the input file may have different lenght.
+            switch (time.Length)
+            {
+                case 4:
+                    hours = Convert.ToInt32(time.Substring(0, 2));
+                    mins = Convert.ToInt32(time.Substring(2, 2));
+                    break;
+                case 3:
+                    hours = Convert.ToInt32(time.Substring(0, 1));
+                    mins = Convert.ToInt32(time.Substring(1, 2));
+                    break;
+                case 1:
+                    hours = 0;
+                    mins = Convert.ToInt32(time.Substring(0, 1));
+                    break;
+            }
+
+            //We represent the clock as values between 0 and 1440, as there are 1440 mins in 24 h.
+            //We return the time as value in this interval.
+            return (hours * 60) + mins;
+
+        }
+    }
+}
